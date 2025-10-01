20a3b46ffad95dc987a75b9dd4ea63ffb98e65b4
Whois <113191030+dratx1@users.noreply.github.com>
2025-10-02 06:08:50 +0700

    Update barbex.py

--- app/menus/barbex.py
+++ app/menus/barbex.py
@@ -185,7 +185,7 @@ def show_barbex_menu2():
             table.add_row(str(idx + 1), p["name"], formatted_price)
 
         table.add_row("00", f"[{theme['text_sub']}]Kembali ke menu sebelumnya[/]", "")
-        table.add_row("99", f"[{theme['text_err']}]Kembali ke menu utama[/]", "")
+        #table.add_row("99", f"[{theme['text_err']}]Kembali ke menu utama[/]", "")
 
         console.print(Panel(
             table,
@@ -197,8 +197,8 @@ def show_barbex_menu2():
         choice = console.input(f"[{theme['text_sub']}]Pilih paket:[/{theme['text_sub']}] ").strip()
         if choice == "00":
             return  # kembali ke menu sebelumnya
-        if choice == "99":
-            return  # keluar ke menu utama
+        #if choice == "99":
+            #return  # keluar ke menu utama
 
         if choice.isdigit() and 1 <= int(choice) <= len(barbex_packages):
             selected_package = barbex_packages[int(choice) - 1]
