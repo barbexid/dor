25b4a03a585e1ac5457d07e542a626fcc1496d9e
Whois <113191030+dratx1@users.noreply.github.com>
2025-10-02 10:52:49 +0700

    Update account2.py

--- app/menus/account2.py
+++ app/menus/account2.py
@@ -106,7 +106,7 @@ def show_account_menu():
 
         if add_user and len(users) >= MAX_FREE_ACCOUNTS and not is_unlocked:
             console.print(Panel(
-                Align.center("🚫 Batas maksimal akun tercapai.\nMasukkan kode unlock untuk menambah lebih banyak akun.", vertical="middle"),
+                Align.center("🚫 Batas maksimal akun sudah tercapai.\nMasukkan kode unlock untuk menambah lebih banyak akun.", vertical="middle"),
                 border_style=theme["border_warning"],
                 padding=(1, 2),
                 expand=True
