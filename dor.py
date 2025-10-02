e7f743e911e4921f3b64d29d0c7e2c5297d9d5a6
Whois <113191030+dratx1@users.noreply.github.com>
2025-10-02 21:25:27 +0700

    Update dor.py

--- dor.py
+++ dor.py
@@ -75,7 +75,7 @@ def show_main_menu(number, balance, display_quota, balance_expired_at):
 
     info_panel = Panel(
         info_table,
-        title=f"[{theme['text_title']}]⚜️ Informasi Akun ⚜️[/]",
+        title=f"[{theme['text_title']}]🛡️Informasi Akun🛡️[/]",
         title_align="center",
         border_style=theme["border_info"],
         padding=(1, 2),
