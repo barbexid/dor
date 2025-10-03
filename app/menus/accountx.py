be704521b1df0ef28868f525d61befae356c06d4
Whois <113191030+dratx1@users.noreply.github.com>
2025-10-03 07:29:44 +0700

    Update accountx.py

--- app/menus/accountx.py
+++ app/menus/accountx.py
@@ -111,10 +111,10 @@ def show_account_menu():
         else:
             for idx, user in enumerate(users):
                 is_active = active_user and user["number"] == active_user["number"]
-                status = "Aktif" if is_active else ""
-                print(f"{idx + 1}. {user['number']} ({status})")
+                status = f" ({'Aktif'})" if is_active else ""
+                print(f"{idx + 1}. {user['number']}{status}")
 
-        print("-------------------------------------------------------")
+        print("----------------------------------")
         print("Command:")
         print("0: Tambah Akun")
         print("00: Kembali ke menu utama")
