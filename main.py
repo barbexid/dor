29f19a479fda7f5e154ec509e13afe15d128d8ac
Whois <113191030+dratx1@users.noreply.github.com>
2025-10-03 07:04:42 +0700

    Update main.py

--- main.py
+++ main.py
@@ -7,7 +7,7 @@ from app.menus.util import clear_screen, pause
 from app.client.engsel import *
 from app.service.auth import AuthInstance
 from app.menus.bookmark import show_bookmark_menu
-from app.menus.account import show_account_menu
+from app.menus.accountx import show_account_menu
 from app.menus.package import fetch_my_packages, get_packages_by_family, show_package_details
 from app.menus.hot import show_hot_menu, show_hot_menu2
 from app.service.sentry import enter_sentry_mode
